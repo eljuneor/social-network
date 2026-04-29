fc4a1b5f7658f988ce6cd0da4c876e674a5bbddb
Jun3or <arzabal.flores.adolfo.m3s2@gmail.com>
2026-04-28 23:29:50 -0600

    forzar autoIndex en mongoose

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     console.log('URI recibida:', process.env.MONGO_URI);
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true
+    });
     console.log('MongoDB conectado');
   } catch (error) {
     console.error('Error de conexión:', error.message);
